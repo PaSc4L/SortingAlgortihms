3e0f0a31b544625b6adc727c397bb02e05f998cd
Csaba Birkás <bcsaba04082@gmail.com>
2023-07-05 00:00:20 +0200

    Insertion sort added
    Insertion sort algorithm added, with comments (the obvious comments will be left soon

--- SortingAlgorithms/src/com/Main.java
+++ SortingAlgorithms/src/com/Main.java
@@ -3,13 +3,14 @@ package com;
 public class Main {
 
     public static void main(String[] args) {
-        Integer[] arr = {64,25,12,22,11};   //random array to sort
-	    //System.out.println(getMinimum(arr));
+        Integer[] arr = {64, 25, 12, 22, 11};   //random array to sort
+        //System.out.println(getMinimum(arr));
         //System.out.println(getMaximum(arr));
 
         //arr=selectionSort(arr);
-        arr=bubbleSort(arr);
-        for (Integer element: arr){
+        //arr = bubbleSort(arr);
+        arr = insertionSort(arr);
+        for (Integer element : arr) {
             System.out.println(element);
         }
     }
@@ -65,26 +66,53 @@ public class Main {
         }
         return list;    //return sorted list
     }
+
+    /**
+     * Bubble sort
+     * @param list
+     * @return sorted array
+     */
     public static Integer[] bubbleSort(Integer[] list){
-        Integer swap;
+        Integer swap;   //temporary integer for storing a value to be swapped
         for (int i =0; i<list.length; i++){
-            for (int j=1; j<list.length-i; j++){
-                if(list[j]<list[j-1]){
-                    swap=list[j-1];
-                    list[j-1] = list[j];
-                    list[j] = swap;
+            for (int j=1; j<list.length-i; j++){    //going trough the list from 1st index to avoid out of bounds sexception
+                if(list[j]<list[j-1]){  //checking if the list's previous element is higher than the current
+                    swap=list[j-1]; //storing previous item to swap
+                    list[j-1] = list[j];    //giving the current item's value to the previous itme
+                    list[j] = swap; //giving the stored value to the current item
                 }
             }
         }
 
         return list;
     }
+
+    /**
+     * Inseton sort
+     * @param list
+     * @return Sorted array
+     */
     public static Integer[] insertionSort(Integer[] list){
+        Integer swap;
+        Integer j=1;    //starting with the first index to avoid out of bounds
+        while(j<list.length){   //using while loop so that I can increment or decrement the j variable any time it needed
+            if(list[j]<list[j-1]){  //change the values if the current is lower than the previous
+                swap = list[j];
+                list[j] = list[j-1];
+                list[j-1] = swap;
+                if(j-1!=0){ //checking if the previous is not the first (to avoid out of bounds sexception)
+                    j --;   //decrement j, to check if the switched value with the previous value(s)
+                }else j++;  //increment j if the switch hapenned with the first element of the array
+            }else j++;  //increase if swapping happened
+        }
         return list;
     }
+
     public static Integer[] mergeSort(Integer[] list){
         return list;
-    }public static Integer[] quickSort(Integer[] list){
+    }
+
+    public static Integer[] quickSort(Integer[] list){
         return list;
     }
 }
